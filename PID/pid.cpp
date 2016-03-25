9079e502cfbe65bd3f3b2be845872c6f6beedb71
Malcolm Ma <malichaooo@gmail.com>
2016-03-25 15:12:57 -0500

    Comment the PID files

--- PID/pid.cpp
+++ PID/pid.cpp
@@ -1,103 +1,121 @@
-/*
- * pid.cpp
- *
- *  Created on: Mar 25, 2016
- *      Author: Lichao
- */
+/******************************************************************************
+Author		: Lichao Ma
+Date  		: Mar 25,2016
+version		: v0.1
+Description :
+	PID controller program for controlling the car
+*****************************************************************************/
+#include <math.h>
+#include <iostream>
+#include <stdio.h>
 #include <numeric>      // std::accumulate
 #include "car.h"
 #include "pid.h"
-#include "math.h"
-#include <iostream>
-#include "stdio.h"
+
 using namespace std;
 
-void PID::update(Car &car,const float target){
-	float error,dError;
-	static float lastError=0;
-	static float integral=0;
 
-	error=target-car.getDistance();
-	dError=error-lastError;
-	integral+=error;
-	constrain(integral,-100,100);
-	lastError=error;
+void PID::update(Car &car, const float target) {
+	float error, dError;
+	static float lastError = 0;
+	static float integral = 0;
+
+	error = target - car.getDistance();
+	dError = error - lastError;
+	integral += error;
+	constrain(integral, -100, 100);
+	lastError = error;
 
 	float output;
-	output=kP*error+kI*integral+kD*dError;
-	constrain(output,-100,100);
+	output = kP * error + kI * integral + kD * dError;
+	constrain(output, -100, 100);
 	car.update(output);
 }
 
 
-void PID::constrain(float &val,const float min,const float max){
-	if(val<min)
-		val=min;
-	if(val>max)
-		val=max;
+void PID::constrain(float &val, const float min, const float max) {
+	if (val < min)
+		val = min;
+	if (val > max)
+		val = max;
 }
 
-float PID::simulate(Car &car,const float target,const size_t simulationTime){
-	float error=0;
-	if(simulationTime==0)
+//Simply simulate the car motion using PID control and return the average error.
+//If you want to record the speed and distance of the car at every time step,
+//use update() and manually record the speed and distance.
+float PID::simulate(Car &car, const float target, const size_t simulationTimes) {
+	float error = 0;
+	if (simulationTimes == 0)
 		return 0;
 
 	car.resetOrigin();
-	for(size_t i=0;i<simulationTime;i++){
-		update(car,target);
-		error+=fabs(target-car.getDistance());
+	for (size_t i = 0; i < simulationTimes; i++) {
+		update(car, target);
+		error += fabs(target - car.getDistance());
 	}
-	return error/simulationTime;
+	return error / simulationTimes;
 }
 
+//PID parameter self optimizing function,parameter description:
+//car	: this program use a one-dimensional car as trail example
+//target: the target position of the car
+//tolerance: the minimal steps to probe the PID parameters
+//simulationTimes: the duration to try out each PID setting
 void PID::twiddle(	Car &car,
 					const float target,
 					const float tolerance,
-					const unsigned int simulationTime){
+					const unsigned int simulationTimes){
 
-	float pidBackup[3]={kP,kI,kD};
-	float pid[3]={kP,kI,kD};
-	float deltaPID[3]={1,1,1};
-	float bestError=simulate(car,target,simulationTime);
+	float pidBackup[3] = { kP, kI, kD };
+	float pid[3] = { kP, kI, kD };		//put it in array to ease the iteration
+	float deltaPID[3] = { 1, 1, 1 };	//The steps it takes to probe the PID values
+	float bestError = simulate(car, target, simulationTimes);
 
-	unsigned int watchDog=1000;	//Prevent dead loop
-	bool success=true;
-	while(accumulate(deltaPID,deltaPID+3,0)>tolerance){
-		if(--watchDog==0){
-			cout<<"twiddle() runtime exceeded!\n"<<endl;
-			success=false;
+	unsigned int watchDog = 1000;	//Prevent dead loop
+	bool success = true;
+
+	//Tolerance is the minimal steps to probe the PID coefficient
+	while (accumulate(deltaPID, deltaPID + 3, 0) > tolerance) {
+		if (--watchDog == 0) {
+			//cout << "twiddle() runtime exceeded!\n" << endl;
+			success = false;
 			break;
 		}
 
-		float error=0;
-		for(size_t i=0;i<3;i++){
-			pid[i]+=deltaPID[i];
-			setPID(pid[0],pid[1],pid[2]);
-			error=simulate(car,target,simulationTime);
-			if(error<bestError){
-				bestError=error;
-				deltaPID[i]*=1.1;
-			}else{
-				pid[i]-=deltaPID[i]*2;
-				setPID(pid[0],pid[1],pid[2]);
-				error=simulate(car,target,simulationTime);
-				if(error<bestError){
-					bestError=error;
-					deltaPID[i]*=1.1;
-				}else{
-					pid[i]+=deltaPID[i];
-					deltaPID[i]*=0.9;
+		float error = 0;
+		for (size_t i = 0; i < 3; i++) {	//Probing P,I,D one by one
+			pid[i] += deltaPID[i];			//First,try increasing the PID
+			setPID(pid[0], pid[1], pid[2]);
+			error = simulate(car, target, simulationTimes);
+
+			//After simulation,we evaluate the performance of the setting
+			if (error < bestError) {		//Quite good,increment the step size
+				bestError = error;
+				deltaPID[i] *= 1.1;
+			} else {
+				pid[i] -= deltaPID[i] * 2;	//Second,try decreasing the PID
+				setPID(pid[0], pid[1], pid[2]);
+				error = simulate(car, target, simulationTimes);
+
+				//After simulation,we evaluate the performance of the setting
+				if (error < bestError) {
+					bestError = error;
+					deltaPID[i] *= 1.1;		//Quite good,increment the step size
+				} else {
+					pid[i] += deltaPID[i];
+					deltaPID[i] *= 0.9;		//Bad move,decrease the step size
 				}
 			}
 		}
 	}
-	if(success){
-		cout<<"twiddle success\n";
-		printf("Optimal PID setting:\nP%.3f\tI%.3f\tD%.3f\n",pid[0],pid[1],pid[2]);
-		setPID(pid[0],pid[1],pid[2]);
-	}
-	else{
-		setPID(pidBackup[0],pidBackup[1],pidBackup[2]);
+
+	//If optimization succeeded,update the pid coefficient,otherwise restore the values
+	if (success) {
+		//cout << "twiddle success\n";
+		//printf("Optimal PID setting:\nP%.3f\tI%.3f\tD%.3f\n", pid[0], pid[1],pid[2]);
+		setPID(pid[0], pid[1], pid[2]);
+	} else {
+		setPID(pidBackup[0], pidBackup[1], pidBackup[2]);
 	}
 
 
--- PID/pid.h
+++ PID/pid.h
@@ -10,27 +10,39 @@
 
 class PID{
 public:
-	void update(Car &car,const float target);
-	PID(float p,float i,float d):kP(p),kI(i),kD(d),output(0){}
+	PID(float p, float i, float d) : kP(p), kI(i), kD(d), output(0) {}
 	void setPID(float p,float i,float d){
 		kP=p;
 		kI=i;
 		kD=d;
 	}
-	float getOutput(){return output;}
+
+	void update(Car &car,const float target);
+
+	float getOutput(){ return output; }
+
+	//Simply simulate the car motion using PID control and return the average error.
+	//If you want to record the speed and distance of the car at every time step,
+	//use update() and manually record the speed and distance.
 	float simulate(Car &car,const float target,const size_t simulationTime);
+
+	//PID parameter self optimizing function,parameter description:
+	//car	: this program use a one-dimensional car as trail example
+	//target: the target position of the car
+	//tolerance: the minimal steps to probe the PID parameters
+	//simulationTimes: the duration to try out each PID setting
 	void twiddle(	Car &car,
 					const float target,
 					const float tolerance,
-					const unsigned int simulationTime);
+					const unsigned int simulationTimes);
 private:
 	float kP;
 	float kI;
 	float kD;
 	float output;
+
+	//Constrain the output of each term
 	void constrain(float &val,const float min,const float max);
 };
-
-
 
 #endif /* PID_H_ */
