b9a032cae3541ec2e4600bce6f79b3dd46268034
Malcolm Ma <malichaooo@gmail.com>
2016-03-31 14:31:24 -0500

    Move CATCH header to 'include' folder

--- Search/GraphSearch/test/MapSearchTest.cpp
+++ Search/GraphSearch/test/MapSearchTest.cpp
@@ -7,7 +7,7 @@ Description :
 *****************************************************************************/
 
 #define CATCH_CONFIG_MAIN// This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#include "..\include\catch.hpp"
 
 #include <vector>
 #include "..\src\BFS.h"
