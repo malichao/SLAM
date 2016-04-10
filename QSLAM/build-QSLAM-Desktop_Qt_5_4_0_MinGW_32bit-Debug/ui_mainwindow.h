395e1477eb837fa75da74bcd0045c4c285d43e6d
Malcolm Ma <malichaooo@gmail.com>
2016-04-10 02:29:20 -0500

    Add more test cases

---
+++ QSLAM/build-QSLAM-Desktop_Qt_5_4_0_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,147 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.4.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionOpen;
+    QAction *actionSearch;
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QGridLayout *layoutTop;
+    QPushButton *buttonSetTarget;
+    QPushButton *buttonSetStart;
+    QSpacerItem *horizontalSpacer;
+    QGraphicsView *view;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QMenu *menuAction;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(1203, 879);
+        actionOpen = new QAction(MainWindow);
+        actionOpen->setObjectName(QStringLiteral("actionOpen"));
+        actionSearch = new QAction(MainWindow);
+        actionSearch->setObjectName(QStringLiteral("actionSearch"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        centralWidget->setEnabled(true);
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(centralWidget->sizePolicy().hasHeightForWidth());
+        centralWidget->setSizePolicy(sizePolicy);
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        layoutTop = new QGridLayout();
+        layoutTop->setSpacing(6);
+        layoutTop->setObjectName(QStringLiteral("layoutTop"));
+        buttonSetTarget = new QPushButton(centralWidget);
+        buttonSetTarget->setObjectName(QStringLiteral("buttonSetTarget"));
+        QIcon icon;
+        icon.addFile(QStringLiteral("resources/target.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        buttonSetTarget->setIcon(icon);
+
+        layoutTop->addWidget(buttonSetTarget, 0, 1, 1, 1);
+
+        buttonSetStart = new QPushButton(centralWidget);
+        buttonSetStart->setObjectName(QStringLiteral("buttonSetStart"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral("resources/start.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        buttonSetStart->setIcon(icon1);
+
+        layoutTop->addWidget(buttonSetStart, 0, 0, 1, 1);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        layoutTop->addItem(horizontalSpacer, 0, 2, 1, 1);
+
+
+        gridLayout->addLayout(layoutTop, 0, 0, 1, 1);
+
+        view = new QGraphicsView(centralWidget);
+        view->setObjectName(QStringLiteral("view"));
+
+        gridLayout->addWidget(view, 1, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 1203, 31));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        menuAction = new QMenu(menuBar);
+        menuAction->setObjectName(QStringLiteral("menuAction"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuBar->addAction(menuAction->menuAction());
+        menuFile->addAction(actionOpen);
+        menuAction->addAction(actionSearch);
+        mainToolBar->addAction(actionOpen);
+        mainToolBar->addAction(actionSearch);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "SLAM Simulator", 0));
+        actionOpen->setText(QApplication::translate("MainWindow", "Open", 0));
+        actionSearch->setText(QApplication::translate("MainWindow", "Search", 0));
+        buttonSetTarget->setText(QApplication::translate("MainWindow", "Set Target", 0));
+        buttonSetStart->setText(QApplication::translate("MainWindow", "Set Start", 0));
+        menuFile->setTitle(QApplication::translate("MainWindow", "File", 0));
+        menuAction->setTitle(QApplication::translate("MainWindow", "Action", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
